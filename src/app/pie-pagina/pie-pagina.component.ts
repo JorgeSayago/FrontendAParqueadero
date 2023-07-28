d329c5b5af275eb868f51c86bbed92522b34d2aa
juanfranqf2002 <juanfranciscoqf@gmail.com>
2023-07-27 23:52:51 -0500

    ConfiguracionServicePersonaYpruebaCrearPersona

--- src/app/pie-pagina/pie-pagina.component.ts
+++ src/app/pie-pagina/pie-pagina.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/domain/persona';
+import {  PersonaService } from 'src/app/services/persona.service';
+
 
 @Component({
   selector: 'app-pie-pagina',
@@ -6,5 +10,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./pie-pagina.component.scss']
 })
 export class PiePaginaComponent {
+
+  persona: Persona = new Persona();
+
+  constructor(private personaService: PersonaService,
+    private router: Router) {
+
+      let params = this.router.getCurrentNavigation()?.extras.queryParams;
+      if(params){
+        console.log(params)
+        this.persona = new Persona()
+        this.persona = params['persona']
+      }
+    }
+
+    guardar(){
+
+    if(!isNaN(Number(this.persona.cedula))&& this.persona.cedula.length  == 10){
+      this.personaService.save(this.persona).subscribe(data => {
+        console.log("resultado WS save", data);
+        //this.router.navigate(['pagina1/Listar'])
+      });
+      this.persona = new Persona()
+      alert("Contacto creado exitosamente")
+    }else{
+      alert("Error la cedula ingresada no es correcta")
+      this.persona = new Persona()
+    }
+    }
 
 }
--- src/app/services/persona.service.ts
+++ src/app/services/persona.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Persona } from '../domain/persona';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonaService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  save(persona: Persona) {
+    return this.http.post<any>("http://localhost:8080/AParqueaderoWS/rs/clientes", persona)
+  }
+
 }
