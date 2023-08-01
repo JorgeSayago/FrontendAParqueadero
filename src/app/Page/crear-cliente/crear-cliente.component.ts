e782736e2b109ac04ab6cec22eafb5b63d824f48
JorgeSayago <130063995+JorgeSayago@users.noreply.github.com>
2023-08-01 00:32:01 -0500

    c

--- src/app/Page/crear-cliente/crear-cliente.component.ts
+++ src/app/Page/crear-cliente/crear-cliente.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Persona } from 'src/app/domain/persona';
+import { PersonaService } from 'src/app/services/persona.service';
 
 @Component({
   selector: 'app-crear-cliente',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class CrearClienteComponent {
 
+  persona: Persona= new Persona();
+
+  constructor(private personaService: PersonaService,
+    private router: Router){
+      let params = this.router.getCurrentNavigation()?.extras.queryParams;
+      if(params){
+        console.log(params)
+        this.persona = new Persona()
+        this.persona = params['persona']
+      }
+
+  }
+  guardar(){
+    console.log(this.persona)
+    if(!isNaN(Number(this.persona.cedula)) && this.persona.cedula.length === 10 ){
+      this.personaService.save(this.persona).subscribe(data => {
+        console.log("Resultado WS SAVE", data);
+        this.router.navigate(["paginas/Lista"])
+      });
+      this.persona=new Persona()
+      alert("Creado exitosamente")
+  }
+  else{
+    alert("error numero de cedula incompleta o incorrecta")
+    }
+this.persona = new Persona()
+}
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearClienteComponent } from './Page/crear-cliente/crear-cliente.component';
+import { CrearCarroComponent } from './Page/crear-carro/crear-carro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"pagina/crearUsuario",component:CrearClienteComponent},
+  { path:"pagina/crearCarro",component:CrearCarroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
